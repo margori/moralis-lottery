ac952140b890f503e4df3f19b1da56f0fe4ec3e7
Marcelo Briones <ing@marcelobriones.com.ar>
2022-04-05 02:15:16 -0300

    Implement enter endpoint

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,22 +7,35 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract Lottery {
     using SafeMath for uint256;
 
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATIN_WINNER
+    }
+
+    LOTTERY_STATE public lotteryState;
+
     AggregatorV3Interface internal ethUsdPriceFeed;
     uint256 usdEntryFee;
+    address payable[] public players;
 
     constructor(address _ethUsdPriceFeed) {
         ethUsdPriceFeed = AggregatorV3Interface(_ethUsdPriceFeed);
         usdEntryFee = 50; // 50 USD
+        lotteryState = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
+        require(lotteryState == LOTTERY_STATE.OPEN);
         require(msg.value >= getEntranceFee(), "Not enough ether to enter");
+
+        players.push(payable(msg.sender));
     }
 
     function getEntranceFee() public view returns (uint256) {
         uint256 precision = 1 * 10**18;
         uint256 price = getLatestEthUsdPrice();
-        uint256 costToEnter = (precision / price) *(usdEntryFee * 10 ** 8);
+        uint256 costToEnter = (precision / price) * (usdEntryFee * 10**8);
         return costToEnter;
     }
 
